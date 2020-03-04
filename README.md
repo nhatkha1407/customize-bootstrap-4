709da12b8c90f497905c2175126517c076061daf
Whoosh <nhatkha1407@gmail.com>
2020-03-04 18:21:52 +0700

    Add example 3

---
+++ README.md
@@ -0,0 +1,26 @@
+### Hi there!
+
+This is some examples for customizing Bootstrap 4
+
+- Example 1: The way to import BS4
+- Example 2: The way to import BS4 using shared components with 2 roles
+- Example 3: An example using *_variables.scss* and little extra code to make components have the same style as [Material Design](https://material-components.github.io/material-components-web-catalog/)
+
+
+### Deploying from Your Local Machine:
+
+```
+npm install
+```
+```
+gulp
+```
+
+### Demo
+https://w-bootstrap4.netlify.com/
+
+
+
+If you have a concern, don't hesitate to contact me.
+
+I hope this will help you. Good day!
--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,17 @@
 const browserSync = require("browser-sync").create();
 const gulp = require("gulp");
 const sass = require("gulp-sass");
-const sassGlob = require('gulp-sass-glob');
-const autoprefixer = require('gulp-autoprefixer');
-rename = require('gulp-rename'),
+const sassGlob = require("gulp-sass-glob");
+const autoprefixer = require("gulp-autoprefixer");
+const rename = require("gulp-rename");
 
 gulp.task("css_example_1", function () {
   return gulp
     .src("src/assets/scss/example_1/app.scss")
     .pipe(sassGlob())
     .pipe(sass().on("error", sass.logError))
-    .pipe(autoprefixer({
-      browsers: ['> 1%']
-    }))
-    .pipe(rename('example_1.css'))
+    .pipe(autoprefixer())
+    .pipe(rename("example_1.css"))
     .pipe(gulp.dest("public/assets/css"))
     .pipe(browserSync.stream());
 });
@@ -23,35 +21,53 @@ gulp.task("css_example_2_client", function () {
     .src("src/assets/scss/example_2/client/app.scss")
     .pipe(sassGlob())
     .pipe(sass().on("error", sass.logError))
-    .pipe(autoprefixer({
-      browsers: ['> 1%']
-    }))
-    .pipe(rename('example_2_client.css'))
+    .pipe(autoprefixer())
+    .pipe(rename("example_2_client.css"))
     .pipe(gulp.dest("public/assets/css"))
     .pipe(browserSync.stream());
 });
-
 
 gulp.task("css_example_2_admin", function () {
   return gulp
     .src("src/assets/scss/example_2/admin/app.scss")
     .pipe(sassGlob())
     .pipe(sass().on("error", sass.logError))
-    .pipe(autoprefixer({
-      browsers: ['> 1%']
-    }))
-    .pipe(rename('example_2_admin.css'))
+    .pipe(autoprefixer())
+    .pipe(rename("example_2_admin.css"))
     .pipe(gulp.dest("public/assets/css"))
     .pipe(browserSync.stream());
 });
 
-gulp.task("default", function () {
+gulp.task("css_example_3", function () {
+  return gulp
+    .src("src/assets/scss/example_3/app.scss")
+    .pipe(sassGlob())
+    .pipe(sass().on("error", sass.logError))
+    .pipe(autoprefixer())
+    .pipe(rename("example_3.css"))
+    .pipe(gulp.dest("public/assets/css"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("images", function() {
+  return gulp.src("src/assets/images/**/*")
+    .pipe(gulp.dest("public/assets/images"))
+})
+
+let css_task = ("css_example_1", "css_example_2_admin", "css_example_2_client", "css_example_3");
+
+gulp.task("watch", function () {
   browserSync.init({
     server: {
         baseDir: "./public"
     }
   });
 
-  gulp.watch("src/assets/scss/**/*", gulp.parallel("css_example_1", "css_example_2_admin", "css_example_2_client"));
-  gulp.watch("public/*.html").on('change', browserSync.reload);
+  gulp.watch("src/assets/scss/**/*",
+    gulp.parallel(css_task)
+  );
+
+  gulp.watch("public/*.html").on("change", browserSync.reload);
 });
+
+gulp.task("default", gulp.parallel(css_task, "watch"));
